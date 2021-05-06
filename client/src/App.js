95f737ac2b30ddce6cb53b0ec0b5acaae2e6bc58
Tarun Punhani <tpunhani@gmail.com>
2021-05-06 00:16:13 -0500

    created react app and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import NavigationBar from './components/js/NavigationBar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
       <NavigationBar/>
-    </div>
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
--- client/src/components/js/NavigationBar.js
+++ client/src/components/js/NavigationBar.js
@@ -1,18 +1,59 @@
-import React from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import '../css/NavigationBar.css'
+
 
 function NavigationBar() {
+
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <=960){
+            setButton(false)
+        }else{
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize', showButton)
+
     return (
         <>
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Dating Services</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Sign In</Nav.Link>
-            <Nav.Link href="#pricing">Register</Nav.Link>
-          </Nav>
-        </Navbar>
-      </>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo'>
+                        Dating Services <i className='fab fa-typo3' />
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                        </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                                Services
+                        </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                                Products
+                        </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'> SIGN UP</Button>}
+                </div>
+            </nav>
+        </>
     )
 }
 
