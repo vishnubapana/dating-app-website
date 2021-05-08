e7c690dd4464cbd3e9393a7aabbe5dcdfdb646ce
Suyog Ghodey <suyogghodey3@gmail.com>
2021-05-08 18:08:15 -0500

    Small fixes

--- client/src/components/js/MyProfile.js
+++ client/src/components/js/MyProfile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container,Row,Col,Form ,Button} from 'react-bootstrap';
 import { getUser } from '../utils/Common';
 import NavigationBarDashboard from "./NavigationBarDashboard"
+import { storage } from '../utils/firebase'
 
 const axios = require('axios');
 
