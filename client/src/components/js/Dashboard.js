413e35848fe4e9e955172768b1f68b013d3df8c2
Tarun Punhani <tpunhani@gmail.com>
2021-05-08 01:58:09 -0500

    Swipe buttons added in dashboard

--- client/src/components/js/Dashboard.js
+++ client/src/components/js/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavigationBarDashboard from "./NavigationBarDashboard"
 import TinderCards from './tinderCards/TinderCards'
+import SwipeButtons from './swipeButtons/SwipeButtons'
 
 function Dashboard() {
     return (
         <>
         <NavigationBarDashboard/>
         <TinderCards />
+        <SwipeButtons />
         </>
     )
 }
